f7ccee54502c261bc118c26420764b387d160712
ning.hang33 <ning.hang@titansoft.com.sg>
2023-11-25 16:38:26 +0800

    add Budget.cs

---
+++ Budget/BudgetKata/BudgetService.cs
@@ -0,0 +1,34 @@
+﻿namespace BudgetKata;
+
+public class BudgetService
+{
+    private IBudgetRepo _budgetRepo;
+
+    public BudgetService(IBudgetRepo budgetRepo)
+    {
+        _budgetRepo = budgetRepo;
+    }
+
+    public decimal Query(DateTime start, DateTime end)
+    {
+        var budgets = _budgetRepo.GetAll();
+
+        foreach (var budget in budgets)
+        {
+            var year = budget.YearMonth.Substring(0,4);
+            var month = budget.YearMonth.Substring(4,2);
+
+            var budgetDate = new DateTime(Int32.Parse(year),Int32.Parse(month),1);
+            
+            
+        }
+
+
+    }
+
+}
+
+public interface IBudgetRepo
+{
+    List<Budget> GetAll();
+}
